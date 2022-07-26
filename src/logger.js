5c6877052e8d6456e8d789c998a4956df6d5366b
Michał Leszczyk <michasko37@gmail.com>
2022-07-26 18:26:16 +0200

    Fix service name for logging

--- src/logger.js
+++ src/logger.js
@@ -5,7 +5,7 @@ const ERROR_LOG_FILE = "logs/error.log";
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  defaultMeta: { service: "dnslink-api" },
+  defaultMeta: { service: "webportal-handshake-api" },
   transports: [new winston.transports.File({ filename: ERROR_LOG_FILE, level: "error" })],
 });
 
